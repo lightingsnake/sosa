1a0d0bd28631f6b51e014f32a34f8e476b19f103
Lighting snake <andres.territoriovirtual@gmail.com>
2014-05-13 16:03:34 -0500

    Maquetación del Home 1

---
+++ app/views/layout.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Sosa</title>
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="assets/css/global.css">
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<nav id="mainMenu">
+					<ul>
+						<li><a id="btnHome" href="/"></a></li>
+						<li><a id="btnContact" href="/contact">Contacto</a></li>
+					</ul>
+				</nav>
+				<img class="col-md-6" src="assets/images/distintivoHorizontal.png" alt="" id="logo">
+			</div>
+			<div id="networks">
+				<div class="container">
+					<nav>
+						<ul>
+							<li><a id="btnFacebook" href="#"></a></li>
+							<li><a id="btnTwitter" href="#"></a></li>
+							<li><a id="btnInstagram" href="#"></a></li>
+							<li><a id="btnYoutube" href="#"></a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<section id="contentHome">
+			<div id="slider">
+				
+
+				<div class="container">
+					<ul>
+						<li><img src="assets/images/slideAHome.jpg" alt=""></li>
+					</ul>
+					<div id="secondaryMenu">
+						<nav id="secondaryOne" class="col-md-4">
+							<ul>
+								<li><a href="">Sonido movil</a></li>
+								<li><a href="">Efectos visuales</a></li>
+							</ul>
+						</nav>
+
+						<nav id="secondaryTwo" class="col-md-4">
+							<ul>
+								<li><a href="">Iluminación</a></li>
+								<li><a href="">Video</a></li>
+								<li><a href="">Minitecas</a></li>
+							</ul>
+						</nav>
+
+						<nav id="secondaryThree" class="col-md-4">
+							<ul>
+								<li><a href="">Sonido</a></li>
+								<li><a href="">Infraestructura</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<div class="container">
+				<div class="col-md-7" id="elevenYears">
+					<img class="col-md-11" src="assets/images/11years.png" alt="">
+					<p class="col-md-12">
+						TODOS LOS DERECHOS RESERVADOS POR SOSAPRODUCCIONES<br>
+						No se permite la reproducción total o parcial del material contenido en este sitio
+					</p>
+				</div>
+				<div id="moreInfo" class="col-md-5">
+					<img src="assets/images/informacionFoot.png" alt="" class="col-md-12">
+					<ul class="col-md-12">
+						<li>
+							Cr 67 b 48 d 132, 050034 Medellín
+						</li>
+
+						<li>
+							Tel: (574) 448 48 65
+						</li>
+
+						<li>
+							Cel. (57) 313 765 27 36
+						</li>
+					</ul>
+					<a href="http://yuxta.net" id="yuxta"></a>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
